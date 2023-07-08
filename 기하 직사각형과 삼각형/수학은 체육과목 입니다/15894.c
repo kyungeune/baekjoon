cf97cbd90e70c251037c638e475531c5726db75d
kyungeune <kyungeunhehe@gmail.com>
2023-07-08 20:36:06 +0900

    h

--- 기하 직사각형과 삼각형/수학은 체육과목 입니다/15894.c
+++ 기하 직사각형과 삼각형/수학은 체육과목 입니다/15894.c
@@ -1,6 +1,17 @@
-#define CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main(void)
 {
+	long long n, ans=0;
 
+	scanf("%llu", &n);
+
+	for (long long i = 0; i < n; i++) {
+		ans += 4 * (i + 1);
+		ans -= i * 4;
+	}
+
+	printf("%llu", ans);
+
+	return 0;
 }
