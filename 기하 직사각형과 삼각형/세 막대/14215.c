a9fe4f8bd939f25ba73e5131d9969120aae26761
kyungeune <kyungeunhehe@gmail.com>
2023-07-11 13:25:12 +0900

    btob

--- 기하 직사각형과 삼각형/세 막대/14215.c
+++ 기하 직사각형과 삼각형/세 막대/14215.c
@@ -5,18 +5,39 @@
 int main(void)
 
 {
-	int A[3] = { 0 };
-	int Equilateral = 0, Isosceles = 0, Scalene = 0, Invalid = 0;
 
-	for (int i = 0; i < 3; i++) //입력받기
+	int A[3];	for (int i = 0; i < 3; i++) //입력받기
+
 	{
+
 		scanf("%d", &A[i]);
+
 		for (int j = 0; j < i; j++) { //크기 순으로 정렬
+
 			if (A[j] > A[i]) {
+
 				int temp = A[j];
+
 				A[j] = A[i];
+
 				A[i] = temp;
+
 			}
+
 		}
+
 	}
+
+
+
+	if (A[1] == A[2] || A[0] + A[1] - 1 > A[2])
+
+		printf("%d", (A[0] + A[1] + A[2]));
+
+	else
+
+		printf("%d", (A[0] + A[1]) * 2 - 1);
+
+
+
 }
