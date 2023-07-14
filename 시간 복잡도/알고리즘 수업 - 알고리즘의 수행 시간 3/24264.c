f6f0c6e333e66334b41e1beff32d05902138d089
kyungeune <kyungeunhehe@gmail.com>
2023-07-14 21:43:34 +0900

    second

--- 시간 복잡도/알고리즘 수업 - 알고리즘의 수행 시간 3/24264.c
+++ 시간 복잡도/알고리즘 수업 - 알고리즘의 수행 시간 3/24264.c
@@ -2,30 +2,46 @@
 
 #include <stdio.h>
 
+/*	MenOfPassion(A[], n) {
+
+sum <- 0;
+
+for i <- 1 to n
+
+	for j <- 1 to n
+
+		sum <- sum + A[i] × A[j]; # 코드1
+
+return sum;
+
+}
+
+*/
+
+long long H(long long A[], long long n)
+
+{
+
+	long long sum = 0;
+
+	for (int i = 0; i < n; i++)
+
+		for (int j = 0; j < n; j++)
+
+			sum += 1;
+
+	return sum;
+
+}
+
 int main(void)
 
 {
 
-	/*	MenOfPassion(A[], n) {
-
-	sum <- 0;
-
-	for i <- 1 to n
-
-		for j <- 1 to n
-
-			sum <- sum + A[i] × A[j]; # 코드1
-
-	return sum;
-
-	}
-
-	*/
-
-	int n;
+	long long A[500000] = { 0 }, n;
 
 	scanf("%d", &n);
 
-	printf("%d\n2", n * n);
+	printf("%llu\n2", H(A, n));
 
 }
