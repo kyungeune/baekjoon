0c2ce35ae31e53d50dfd5a1cc25bcd6bfbe50c69
kyungeune <kyungeunhehe@gmail.com>
2023-07-17 22:40:54 +0900

    f

--- 시간 복잡도/알고리즘 수업 - 알고리즘의 수행 시간 4/24265.c
+++ 시간 복잡도/알고리즘 수업 - 알고리즘의 수행 시간 4/24265.c
@@ -1,15 +1,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-int MenOfPassion(int A[], int n) {
-    int sum = 0;
-    for (int i = 0; i < n - 1; i++)
-        for (int j = i; j < n; j++)
+/*
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n - 1
+        for j <- i + 1 to n
+            sum <- sum + A[i] × A[j]; # 코드1
+    return sum;
+}
+
+==
+
+n*(n-1)/2
+*/
+long long MenOfPassion(int A[], long long n) {
+    long long sum = 0;
+    for (long long i = 1; i <= n - 1; i++) //n번
+        for (long long j = i + 1; j <= n; j++) //n번
             sum += 1;
+    //최종적으로 n제곱번
     return sum;
 }
 int main(void)
 {
-    int A[500000] = { 0 }, i, n, x = 1;
-    scanf("%d", &n);
-    printf("%d\n2", MenOfPassion(A,n));
+    int A[1] = { 0 };
+    long long n, x = 1;
+    scanf("%llu", &n);
+    printf("%llu\n2", (n-1)*(n)/2);
 }
---
+++ 시간 복잡도/알고리즘 수업 - 알고리즘의 수행 시간 5/24266.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+/*
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n //1번
+        for j <- 1 to n //2번
+            for k <- 1 to n //3번
+                sum <- sum + A[i] × A[j] × A[k]; # 코드1
+    return sum;
+}
+*/
+int main(void)
+{
+    long long n;
+    scanf("%llu", &n);
+    if(n==1)
+        printf("1\n3");
+    else
+        printf("%llu\n3", n * n * n);
+   
+}
