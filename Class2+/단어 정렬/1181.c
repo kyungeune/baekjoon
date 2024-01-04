adbda7b02e54faa2d34accfa7cc563b580fe7f08
kyungeune <132311633+kyungeune@users.noreply.github.com>
2024-01-04 16:07:10 +0900

    국수먹고싶다

--- Class2+/단어 정렬/1181.c
+++ Class2+/단어 정렬/1181.c
@@ -2,33 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 int main(void)
 {
-	int n = 0;
-	char** s = (char**)malloc(sizeof(char*) * n);
-	int len[50][50] = { 0 }; // 길이, 해당하는 i값
-	// 입력
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		s[i] = (char*)malloc(sizeof(char) * 51);
-		scanf("%s", s[i]);
-		len[strlen(s[i])][i] = 1;
-	}
-	// 수정
-	for (int i = 0; i < 50; i++) {
-		for (int j = 0; j < 50; j++) {
-			if (len[i][j] == 0) {
-				break;
-			}
-			else {
-				
-			}
-		}
-	}
-
-
-	// 출력
-	for (int i = 0; i < n; i++) {
-		
-	}
+	
 }
--- Class2+/수 찾기/1920.c
+++ Class2+/수 찾기/1920.c
@@ -3,31 +3,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+int compare(const void* a, const void* b)
+{
+	if (*(int*)a > *(int*)b)
+		return 1;
+	else if (*(int*)a < *(int*)b)
+		return -1;
+	else
+		return 0;
+}
+
+int search(long long save[], int st, int ed, long long find)
+{
+	int mid = -1;
+
+	while (st <= ed) {
+		mid = (st + ed) / 2;
+
+		if (save[mid] >= find) {
+			if (save[mid] == find)
+				return 1;
+			ed = mid - 1;
+		}
+		else {
+			st = mid + 1;
+		}
+	}
+	return 0;
+}
 int main(void)
 {
 	int n = 0, m = 0;
 	scanf("%d", &n);
 	long long* save = (long long*)malloc(sizeof(long long) * n);
 	for (int i = 0; i < n; i++) {
-		long long k;
-		scanf("%lld", &k);
-		save[i] = k;
+		scanf("%lld", &save[i]);
 	}
-
+	// save 정렬
+	qsort(save, n, sizeof(long long), compare); // stdlib 헤더파일에 존재, 비교함수만 추가적으로 짜면 됨
+	
+	// save 탐색 결과 출력
 	scanf("%d", &m);
 	for (int i = 0; i < m; i++) {
-		long long k = 0;
-		int flag = 0;
+		long long k;
 		scanf("%lld", &k);
-		for (int j = 0; j < n; j++) {
-			if (save[j] == k) {
-				flag = 1;
-				printf("1\n");
-				break;
-			}
-		}
-		if (flag == 0)
-			printf("0\n");
+		printf("%d\n", search(save, 0, n - 1, k));
 	}
 
 	free(save);
