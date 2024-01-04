e60565e704f19fcdaa79f3689c19451df6f60574
kyungeune <132311633+kyungeune@users.noreply.github.com>
2024-01-04 22:14:09 +0900

    바이오하자드

--- Class2+/단어 정렬/1181.c
+++ Class2+/단어 정렬/1181.c
@@ -2,8 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int compare(const void* a, const void* b)
+{
+	const char* str1 = *(const char**)a;
+	const char* str2 = *(const char**)b;
+
+	int aLen = strlen(str1);
+	int bLen = strlen(str2);
+
+	if (aLen < bLen)
+		return -1;
+	else if (aLen > bLen)
+		return 1;
+	else
+		return strcmp(str1, str2);
+}
+int is_exist(char** s, int n, char* this, int k)
+{
+	for (int i = k + 1; i < n; i++)
+	{
+		if (strcmp(s[i], this) == 0)
+			return 1;
+	}
+	return 0;
+}
 
 int main(void)
 {
-	
+	int n = 0;
+	scanf("%d", &n);
+	char** s = (char**)malloc(sizeof(char*) * n);
+	for (int i = 0; i < n; i++)
+		s[i] = (char*)malloc(sizeof(char) * 51);
+
+	for (int i = 0; i < n; i++)
+		scanf("%s", s[i]);
+
+	qsort(s, n, sizeof(char*), compare);
+	for (int i = 0; i < n; i++) {
+		if (is_exist(s, n, s[i], i) == 0)
+			printf("%s\n", s[i]);
+	}
+
+	for (int i = 0; i < n; i++)
+		free(s[i]);
+	free(s);
 }
