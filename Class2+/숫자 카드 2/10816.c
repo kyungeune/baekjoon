0097e7dcc3b7e8d99f116fd5da7106d712f7b93c
kyungeune <132311633+kyungeune@users.noreply.github.com>
2024-01-08 21:26:51 +0900

    내남편과결혼해줘개꿀잼

---
+++ Class2+/숫자 카드 2/10816.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct {
+	int cnt;
+}Have;
+int main(void)
+{
+	int n = 0, m = 0;
+	scanf("%d", &n);
+	Have* h = (Have*)malloc(sizeof(Have) * 20000001);
+	for (int i = 0; i < 20000001; i++)
+		h[i].cnt = 0;
+
+	for (int i = 0; i < n; i++) {
+		int imsi;
+		scanf("%d", &imsi);
+		if (imsi < 0) {
+			imsi += 10000000;
+			h[imsi].cnt++;
+		}
+		else
+			h[imsi].cnt++;
+	}
+	
+	scanf("%d", &m);
+	for (int i = 0; i < m; i++) {
+		int imsi;
+		scanf("%d", &imsi);
+		if (imsi < 0) {
+			imsi += 10000000;
+			printf("%d ", h[imsi].cnt);
+		}else
+			printf("%d ", h[imsi].cnt);
+	}
+
+	free(h);
+	return 0;
+}
