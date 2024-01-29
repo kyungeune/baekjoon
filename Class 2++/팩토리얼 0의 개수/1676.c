81636f384f5f4f386d815cb20fcb01a8c7470ec5
kyungeune <132311633+kyungeune@users.noreply.github.com>
2024-01-29 15:31:12 +0900

    yeah

--- Class 2++/팩토리얼 0의 개수/1676.c
+++ Class 2++/팩토리얼 0의 개수/1676.c
@@ -2,5 +2,32 @@
 #include <stdio.h>
 int main(void)
 {
+	long long n, sum = 0, e = 0, o = 0;
+	
+	scanf("%lld", &n);
 
+	for (int i = 1; i <= n; i++) {
+		int imsi = i;
+		if (imsi % 2 == 0) {
+			while (imsi >= 2 && imsi % 2 == 0) {
+				imsi /= 2;
+				e++;
+			}
+		}
+		imsi = i;
+		if (imsi % 5 == 0) {
+			while (imsi >= 5 && imsi % 5 == 0) {
+				imsi /= 5;
+				o++;
+			}
+		}
+	}
+
+	while (e > 0 && o > 0) {
+		sum++;
+		e--;
+		o--;
+	}
+
+	printf("%lld", sum);
 }
