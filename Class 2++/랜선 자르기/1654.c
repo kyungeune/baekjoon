b72c67cc1585d0241b1d5b5d50016600bdb1ea12
kyungeune <132311633+kyungeune@users.noreply.github.com>
2024-01-29 13:24:18 +0900

    부녀회장

--- Class 2++/랜선 자르기/1654.c
+++ Class 2++/랜선 자르기/1654.c
@@ -2,54 +2,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void compare(const void* a, const void* b)
+long long compare(const void* a, const void* b)
 {
-	return (*(int*)a - *(int*)b);
+	return (*(long long*)a - *(long long*)b);
+}
+long long binarySearch(long long save[], long long st, long long ed, long long find)
+{
+	long long mid = -1;
+
+	while (st <= ed) {
+		mid = (st + ed) / 2;
+
+		if (save[mid] >= find) {
+			if (save[mid] == find)
+				return 1;
+			ed = mid - 1;
+		}
+		else
+			st = mid + 1;
+	}
+	return 0;
 }
 // 2 4 50 200
 // 2 2 4 1
+// 2 2 600 1000
+// 2 2 600 1200
+// 4 4 50 50 50 50
+// 2 3 6 7 => 3 3 3 3
+// 3 7 87 67 97
+// 5 4 2147483645 2147483645 2147483645 2147483645 2147483646
 // N개보다 많이 만드는 것도 N개를 만드는 것에 포함된다. 
-int main(void)
+long long main(void)
 {
-	int n = 0, k = 0, end = 0, max = 0, minus = 0;
-	int* l = (int*)malloc(sizeof(int) * 1000000);
+	long long n = 0, k = 0, end = 0, max = 0, same = 0, minus = 0;
+	long long* l = (long long*)malloc(sizeof(long long) * 1000000);
 
-	scanf("%d %d", &n, &k);
+	scanf("%lld %lld", &n, &k);
 
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &l[i]);
+	for (long long i = 0; i < n; i++) {
+		scanf("%lld", &l[i]);
 	}
 
-	qsort(l, n, sizeof(int), compare);
+	qsort(l, n, sizeof(long long), compare);
 
-	int a = l[0];
+	long long a = l[0];
 
-	while (a<=l[0] && a <= l[n - 1]) {
-		int sum = 0;
+	while (a > 0) {
+		long long sum = 0;
 
-		for (int i = 0; i < n; i++) {
+		for (long long i = 0; i < n; i++) {
 			sum += l[i] / a;
 		}
 
-		if (sum >= k) {
-			end = 1;
+		if (sum == k) {
+			same = 1;
 			if (max < a)
 				max = a;
+
 			if (minus == 1)
 				a--;
 			else
 				a++;
 		}
+		else if (sum > k) {
+			if (same == 1)
+				break;
+
+			if (a == l[0] && same == 0) { // 초기
+				max = a;
+				break;
+			}
+			if (minus == 1 && same == 0) {
+				max = a;
+				break;
+			}
+			a++;
+		}
 		else {
+			if (same == 1)
+				break;
 			a--;
 			minus = 1;
-			if (end == 1)
-				break;
 		}
 	}
 
-	printf("%d", max);
-	
+	printf("%lld", max);
+
 	free(l);
 
 	return 0;
---
+++ Class 2++/부녀회장이 될테야/2275.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define SIZE 15
+#include <stdio.h>
+int main(void)
+{
+	int t, k, n;
+	int a[SIZE][SIZE] = { 0 };
+
+	for (int i = 0; i < SIZE; i++) {
+
+		for (int j = 0; j < SIZE; j++) {
+			if (i == 0)
+				a[i][j] = j + 1;
+			else {
+				for (int k = 0; k <= j; k++) {
+					a[i][j] += a[i - 1][k];
+				}
+			}
+			printf("%d", a[i][j]);
+		}
+		printf("\n");
+
+	}
+
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++) {
+		scanf("%d %d", &k, &n);
+		printf("%d \n", a[k][n - 1]);
+	}
+
+	return 0;
+}
