bc8b01a187e76f1d9bba8e0741359de9450f7c46
kyungeune <132311633+kyungeune@users.noreply.github.com>
2024-09-04 23:20:35 +0900

    e

--- Class2+/웰컴키트/30802.c
+++ Class2+/웰컴키트/30802.c
@@ -7,45 +7,23 @@ int main(void)
 
 	scanf("%d", &n);
 
-	int* k = (int*)malloc((n+1) * sizeof(int));
+	int k[6] = { 0 };
 
-	for (int i = 0; i < n + 1; i++) // 초기화
-		k[i] = 0;
-
-	for (int i = 0; sum < n;) { // 지금까지 입력받은 것의 합계(sum)가 n미만일때 계속 입력받기
-		scanf("%d", &m); // 입력
-
-		if (m != 0) { // 0이 아닌 경우에만 입력받기
-			k[cnt++] = m; // cnt = 입력받은 것의 개수(0 제외)
-		}
+	for (int i = 0; i < 6; i++) {
+		scanf("%d", &k[i]);
 	}
 
 	scanf("%d %d", &t, &p);
 	
-	for (int i = 0; i < cnt; i++) {
-		int imsi = k[i];
-
-		if (imsi < t)
+	for (int i = 0; i < 6; i++) {
+		tn += ((k[i] + t) / t - 1);
+		if (k[i] % t != 0) 
 			tn++;
-		else {
-			tn += (int)(imsi / t); // 0이 아닌 것들을 입력받았기 때문에 
-			if (imsi % t != 0) // 나머지가 존재할 경우(=t의 배수가 아닐 경우)
-				tn++;
-		}
-
 	}
 
 	printf("%d\n", tn);  // t개수, pen개수, 나머지개수
 
-	if (n / p < 1)
-		printf("1 ");
-	else
-		printf("%d ", (int)(n / p));
-
-	printf("%d", n % p);
-
-
-	free(k);
+	printf("%d %d", (n + p) / p - 1, (n + p) % p);
 
 	return 0;
 }
